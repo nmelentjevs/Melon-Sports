c3e67bfdb06865d7f5fd707865953fd63d46306d
Kingusha23 <kingusha2333@gmail.com>
2019-04-05 00:23:03 +0100

    fixes

---
+++ build/precache-manifest.bbe2e7df9d7d63eef8dfbff0f53c8fef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d71fc862ffef40e6ba9b",
+    "url": "/static/js/main.010184f0.chunk.js"
+  },
+  {
+    "revision": "ac81cddbf758e1465713",
+    "url": "/static/js/2.4c05c09f.chunk.js"
+  },
+  {
+    "revision": "d71fc862ffef40e6ba9b",
+    "url": "/static/css/main.d10130f2.chunk.css"
+  },
+  {
+    "revision": "6b65fbe6ca81d767da64286b48410c98",
+    "url": "/index.html"
+  }
+];
