537df149f1ee85d717f141b532beeeebea7a3034
Kingusha23 <kingusha2333@gmail.com>
2019-04-05 01:35:35 +0100

    fixes

--- src/config/keys.js
+++ src/config/keys.js
@@ -1,5 +1,10 @@
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./keys_prod');
-} else {
-  module.exports = require('./keys_dev');
-}
+// if (process.env.NODE_ENV === 'production') {
+//   module.exports = require('./keys_prod');
+// } else {
+//   module.exports = require('./keys_dev');
+// }
+
+module.exports = {
+  oddsKey: 'fa4c4e4c01e33f6ab7d836beb2f23492',
+  footballAPI: '918929b988b74671a7f8bba2cd0cde8d'
+};
---
+++ src/config/keys_dev.js
@@ -0,0 +1,4 @@
+module.exports = {
+  oddsKey: 'fa4c4e4c01e33f6ab7d836beb2f23492',
+  footballAPI: '918929b988b74671a7f8bba2cd0cde8d'
+};
